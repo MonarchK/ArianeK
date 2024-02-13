8f2e618b2481d09d73ae09c2f8e6821d22fc70bb
MonarchK <103085436+MonarchK@users.noreply.github.com>
2024-02-13 11:13:39 -0500

    Update Exhibition

--- src/views/Exhibition.vue
+++ src/views/Exhibition.vue
@@ -1,3 +1,68 @@
 <template>
-  
+  <body>
+  <div class="my-auto mx-auto border-top border-bottom border-info text-center text-light bg-dark hi">
+    <div class="p-5">
+      <div class="down">
+        <p class="display-2">
+          <a target="_blank" href="https://www.instagram.com/theprincessmami?igsh=NTJqc3FnOGRibXEy" class="text-decoration-none">
+            Princess Mami
+          </a>
+        </p>
+      </div>
+      <div class="row">
+        <div class="col-sm mt-5 mt-lg-0 col-md-12 pt-md-4 col-lg-4 pt-lg-0">
+           <a target="_blank" href="https://www.instagram.com/p/C2z9XnOOhjX/?igsh=dXNnbzVvOHl3dHI5"><img class="w-50" src="../img/PCD/pm/pm1.jpg" /></a>
+        </div>
+        <div class="col-sm mt-5 mt-lg-0 col-md-12 pt-md-4 col-lg-4 pt-lg-0">
+          <img class="w-50" src="../img/PCD/pm/pm2.jpg" />
+        </div>
+        <div class="col-sm mt-5 mt-lg-0 col-md-12 pt-md-4 col-lg-4 pt-lg-0">
+          <img class="w-50" src="../img/PCD/pm/pm3.jpg" />
+        </div>
+      </div>
+    </div>
+    <div class="p-5">
+      <div class="down">
+        <p class="display-2"><a target="_blank" href="https://www.instagram.com/tit4tats__?igsh=a3p1emIwaHhvanh5" class="text-decoration-none">TIT4TATS</a>
+          <!--<router-link to="/careers/wardrobe" class="text-decoration-none">
+            Fashion Designing
+          </router-link>-->
+        </p>
+      </div>
+      <div class="row">
+        <div class="col-sm mt-5 mt-lg-0 col-md-12 pt-md-4 col-lg-4 pt-lg-0">
+          <a target="_blank" href="https://www.instagram.com/p/C22tsYbtn6_/?igsh=M3NxeGtuOG5qNWph"><img class="img-fluid" src="../img/PCD/t4t/t4t1.jpg" /></a>
+        </div>
+        <div class="col-sm mt-5 mt-lg-0 col-md-12 pt-md-4 col-lg-4 pt-lg-0">
+           <a target="_blank" href="https://www.instagram.com/p/C3DrpTHt4XY/?igsh=MnFnaHF3YWwwbXVo"><img class="img-fluid" src="../img/PCD/t4t/t4t2.jpg" /></a>
+        </div>
+        <div class="col-sm mt-5 mt-lg-0 col-md-12 pt-md-4 col-lg-4 pt-lg-0">
+           <a target="_blank" href="https://www.instagram.com/p/C3IrmJKNki8/?igsh=MXU2MjdmbjVxNnR4ag=="><img class="img-fluid" src="../img/PCD/t4t/t4t3.jpg" /></a>
+        </div>
+      </div>
+    </div>
+  </div>
+  </body>
 </template>
+
+<style scoped>
+div{
+  margin-right: auto;
+  margin-left: auto;
+}
+  
+.hi{
+  max-width: fit-content;
+}
+html, body{
+  background-color: black;
+}
+.down{
+  margin-top: 5%;
+  margin-bottom: 5%;
+}
+
+*{
+  background-color: black;
+}
+</style>
