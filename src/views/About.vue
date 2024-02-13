e12f558b336c7eec5405b6e4bf17a3a52a59b371
MonarchK <103085436+MonarchK@users.noreply.github.com>
2024-02-13 03:11:31 -0500

    Update About

--- src/views/About.vue
+++ src/views/About.vue
@@ -4,16 +4,17 @@
         <div class="text-center hi">
           <div class="box bg-light text-dark border">
             <h2 class="fst-italic">About me</h2>
-            <p>As a self-employed fashion stylist and photographer,
-             I combine my creative skills and business acumen to create stunning and engaging visual content for various clients and platforms.
-              I have been working in this field for over a year and a half, and I have developed a diverse portfolio of projects,
-               ranging from editorial shoots to personal branding campaigns. I have also gained proficiency in multiple foreign languages,
-                which enables me to communicate effectively with different audiences and cultures. 
-                Additionally, I have strong people management skills, as I often collaborate with models, designers, makeup artists, 
-                and other professionals in the fashion industry. Alongside my freelance work, 
-                I am pursuing a Bachelor of Laws degree at Queen Mary University of London, 
-                where I am learning about the legal aspects of various fields, including fashion, media, and intellectual property. 
-                I am passionate about fashion and I wish to write my own visual stories about it.
+            <p>Ariane Kayla is a stylist known for her work in the fashion industry. Here are some details about her:<br />
+              Hypebeast Magazine Interview:<br />
+              Ariane Kayla was featured in an in-depth interview in Hypebeast Magazine Issue 32: The Fever Issue. She is a stylist who has made significant contributions to London’s avant-garde fashion community.
+              Her work is embodied by designer Nasir Mazhar’s Fantastic Toiles collective.
+              Ariane draws inspiration from London’s underground party scene, where creativity thrives in unexpected rave-friendly hubs and high-tempo warehouse nights.
+              She collaborates with headwear designer Benny Andallo, whose visionary hats transform wearers into something fabulous and authoritative, regardless of their social or economic status.
+              Notable figures like Rihanna, Pink Pantheress, Kid Cudi, FKA twigs, and Junya Watanabe have embraced Andallo’s silhouettes, which blend the past with a fearless futuristic aesthetic.<br />
+              Photographer and Creative Director:<br />
+              Ariane Kayla is also an American-born Photographer and Creative Director based in London.
+              Her multicultural background, having grown up in different parts of the world, informs her artistic palette and cultural knowledge.
+              She aims to reflect this diversity in every piece of art she creates.
             </p>
           </div>
       <div text-center>
